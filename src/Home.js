d530bb450f1e0c0846172e1a555728c0df6303e1
Tanyu Brandon <130669586+brandontanyu@users.noreply.github.com>
2023-09-20 06:47:25 +0100

    added style to body mapped template

--- src/Home.js
+++ src/Home.js
@@ -2,19 +2,20 @@ import { useState } from "react";
 
 const Home = () => {
     //making a reative value with usestate
-    let [name,setName] = useState("Brandon");
-    let [age,setAge] = useState(25);
-  
-    let handleClick = () => {
-      setName("Tanyu");
-      setAge(22);
-    };
+    let [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
   
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-              <p>{name} is {age} years old!</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+               <div className="preview-blog" key={blog.id}>
+                <h1>{blog.title}</h1>
+                <p>Writen by {blog.author}</p>
+               </div>
+            ))}
         </div>
      );
 }
