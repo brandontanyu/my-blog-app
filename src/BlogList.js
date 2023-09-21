56e446e5d071bcc00d19074eb7c5bcbf22de641b
Tanyu Brandon <130669586+brandontanyu@users.noreply.github.com>
2023-09-21 07:13:55 +0100

    reused componenets

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,8 @@
-const BlogList = ({blogs}) => {
+const BlogList = ({blogs,title}) => {
 
     return ( 
         <div className="blog-list">
+             <h1>{title}</h1>
               {blogs.map((blog) => (
                <div className="preview-blog" key={blog.id}>
                 <h1>{blog.title}</h1>
